6c16b927fa37cc0c9616a976abd5d7b3f5f51b63
Edgar Onghena <dev@edgar.bzh>
2021-07-02 13:33:18 +0200

    :arrow_up: Upgrade dependencies

--- bundler.js
+++ bundler.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const moduleName = require('./package.json').name;
 
-const quietRepo = 'https://raw.githubusercontent.com/quiet/quiet-js/708084716f20d21c420df18ed8812a72105e8783/';
+const quietRepo = 'https://raw.githubusercontent.com/quiet/quiet-js/72782542a41f1b615a02c2ab43a0edb56edb6ce4/';
 
 const requirements = {
     quietEs: quietRepo + 'quiet-emscripten.js',
